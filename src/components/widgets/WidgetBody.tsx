a85ba0c52ceaa44758bfa4e25642158d990c0c0b
VladStarostenko <vlad.starostenko@data-lake.co>
2022-11-28 14:52:50 +0100

    Add animation when widgets are opening and closing

--- src/components/widgets/WidgetBody.tsx
+++ src/components/widgets/WidgetBody.tsx
@@ -10,7 +10,13 @@ interface Props {
 
 export const WidgetBody = ({ isOpen, children, onClick }: Props) => (
     <div className="w-full flex flex-col items-center bg-black-600 rounded-[16px] my-6 box-shadow">
-        <div className="w-full">{isOpen ? children : ''}</div>
+        <div
+            className={`w-full transition-opacity duration-300 ${
+                isOpen ? 'h-auto opacity-100' : 'h-0 opacity-0 overflow-hidden'
+            }`}
+        >
+            {children}
+        </div>
         <div
             className="w-full h-10 flex justify-center items-center cursor-pointer"
             onClick={onClick}
