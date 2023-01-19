7b77084f44025b09b81d28affba438b898bcd716
VladStarostenko <vlad.starostenko@data-lake.co>
2023-01-19 22:47:06 +0800

    Adjust PDF for mobile view

--- src/components/pdfViewer/PdfViewer.tsx
+++ src/components/pdfViewer/PdfViewer.tsx
@@ -21,17 +21,19 @@ export const PdfViewer = ({ file }: Props) => {
     return (
         <>
             {isLoading && <Loading />}
-            <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
-                {Array.from(new Array(numPages), (el, index) => (
-                    <Page
-                        key={`page_${index + 1}`}
-                        pageNumber={index + 1}
-                        renderTextLayer={false}
-                        renderAnnotationLayer={false}
-                        className="flex justify-center"
-                    />
-                ))}
-            </Document>
+            <div className="flex justify-start sm:justify-center">
+                <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+                    {Array.from(new Array(numPages), (el, index) => (
+                        <Page
+                            key={`page_${index + 1}`}
+                            pageNumber={index + 1}
+                            renderTextLayer={false}
+                            renderAnnotationLayer={false}
+                            className="flex justify-center"
+                        />
+                    ))}
+                </Document>
+            </div>
         </>
     );
 };
